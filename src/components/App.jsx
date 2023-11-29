b432c5b9922b453f981721c7ca67069e7e305bc3
Ivan <ivanvasyunin@gmail.com>
2023-11-29 16:01:56 +0100

    initial code added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,11 @@
-export const App = () => {
+import Users from './Users';
+
+function App() {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Users />
     </div>
   );
-};
+}
+
+export default App;
